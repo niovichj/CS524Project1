b86d9f4fa42e8381d9af7eb9f751723189cf1bc6
ahood528 <44651640+ahood528@users.noreply.github.com>
2021-11-02 01:02:10 -0400

    Making it so future commits won't have to adjust this line

--- covidcare/covidcare/settings.py
+++ covidcare/covidcare/settings.py
@@ -55,7 +55,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-			BASE_DIR / "mainpage/templates"
+			BASE_DIR / "mainpage/templates",
 		],
         'APP_DIRS': True,
         'OPTIONS': {
